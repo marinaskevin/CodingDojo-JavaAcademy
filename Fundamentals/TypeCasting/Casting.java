ad5a1f2398402070b2fb5977a2b75c31f758bc2c
Kevin Marinas <marinaskevin@outlook.com>
2019-07-08 09:53:34 -0700

    Created and compiled Casting.java

---
+++ Fundamentals/TypeCasting/Casting.java
@@ -0,0 +1,15 @@
+class Casting {
+	public static void main(String[] args) {
+		double d = 35.25;
+		double dd = 35.99;
+
+		// casting the double d into an int
+		int i = (int) d;
+
+		// casting the double dd into an int
+		int ii = (int) dd;
+
+		System.out.println(i);
+		System.out.println(ii);
+	}
+}
