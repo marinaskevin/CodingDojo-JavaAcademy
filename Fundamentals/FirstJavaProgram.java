95a1584937464996e87b92027fc3cea409e28065
Kevin Marinas <marinaskevin@outlook.com>
2019-07-08 09:29:20 -0700

    Created and compiled FirstJavaProgram.java

---
+++ Fundamentals/FirstJavaProgram.java
@@ -0,0 +1,9 @@
+public class FirstJavaProgram
+{
+	public static void main(String[] args)
+	{
+		System.out.println("My name is Kevin Marinas");
+		System.out.println("I am 32 years old");
+		System.out.println("My hometown is Seattle, WA");
+	}
+}
