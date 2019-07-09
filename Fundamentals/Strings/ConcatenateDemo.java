30b78007b5d7de52b822ec54ac0e76c3318609e2
Kevin Marinas <marinaskevin@outlook.com>
2019-07-09 11:00:54 -0700

    Created and compiled ConcatenateDemo.java

---
+++ Fundamentals/Strings/ConcatenateDemo.java
@@ -0,0 +1,9 @@
+public class ConcatenateDemo {
+	public static void main(String[] args) {
+		String string1 = "My name is ";
+		String string2 = "Kevin";
+		String string3 = string1.concat(string2);
+		System.out.println(string3);
+		// will output My name is Kevin
+	}
+}
