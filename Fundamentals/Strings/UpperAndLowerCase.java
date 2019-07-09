474c80c6ea069dc255be0f1016abf6a4f8c5802d
Kevin Marinas <marinaskevin@outlook.com>
2019-07-09 12:53:06 -0700

    Created and compiled UpperAndLowerCase.java

---
+++ Fundamentals/Strings/UpperAndLowerCase.java
@@ -0,0 +1,8 @@
+public class UpperAndLowerCase {
+	public static void main(String[] args) {
+		String a = "HELLO";
+		String b = "world";
+		System.out.println(a.toLowerCase()); // hello
+		System.out.println(b.toUpperCase()); // WORLD
+	}
+}
