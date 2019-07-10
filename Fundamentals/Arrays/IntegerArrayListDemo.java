aef92667c7ced86656fd43105c67a459bbee107d
Kevin Marinas <marinaskevin@outlook.com>
2019-07-10 12:54:12 -0700

    Created and compiled IntegerArrayListDemo.java

---
+++ Fundamentals/Arrays/IntegerArrayListDemo.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class IntegerArrayListDemo {
+	public static void main(String[] args) {
+		ArrayList<Integer> myArray = new ArrayList<Integer>();
+		myArray.add(10);
+		int num = myArray.get(0);
+		System.out.println(num);
+	}
+}
