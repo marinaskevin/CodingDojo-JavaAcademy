2758e767ae4dc248c120680f3b43cfaeb67a1bb8
Kevin Marinas <marinaskevin@outlook.com>
2019-07-11 23:49:11 -0700

    Created and compiled TracksAndLyricsDemo.java

---
+++ Fundamentals/Maps/TracksAndLyrics.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+import java.util.Set;
+
+public class TracksAndLyrics {
+	public static HashMap<String, String> getTrackList() {
+		HashMap<String, String> trackList = new HashMap<String, String>();
+		trackList.put("Do","Re Re");
+		trackList.put("Mi","Fa Fa");
+		trackList.put("So","La La");
+		trackList.put("Ti","Do Do");
+		return trackList;
+	}
+}
