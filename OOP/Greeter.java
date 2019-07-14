1af549340f5fad1ad7600b7788368aa769652bb1
Kevin Marinas <marinaskevin@outlook.com>
2019-07-13 20:37:18 -0700

    Updated the GreeterTest program to include private and static methods

--- OOP/Greeter.java
+++ OOP/Greeter.java
@@ -1,8 +1,18 @@
 public class Greeter {
     public String greet(String name){
-        return "Hello " + name;
+        return createGreeting(name);
     }
     public String greet(){
-        return "Hello World";
+        return createGreeting("World");
+    }
+    public String greet(String firstName, String lastName){
+        System.out.println("We are about to greet " + firstName);
+        return createGreeting(firstName + " " + lastName);
+    }
+    private String createGreeting(String toBeGreeted){
+        return "Greetings " + toBeGreeted + ", welcome to Coding Dojo!";
+    }
+    public static void testGreeting(String name){
+        System.out.println("This is a test of a static method, " + name);
     }
 }
--- OOP/GreeterTest.java
+++ OOP/GreeterTest.java
@@ -1,9 +1,10 @@
 public class GreeterTest {
     public static void main(String[] args){
+        Greeter.testGreeting("Kelvin");
         Greeter g = new Greeter();
         String greeting = g.greet();
         String greetingWithName = g.greet("Eduardo");
-        if (greeting.equals("Hello World") || greetingWithName.equals("Hello Eduardo")){
+        if (greeting.equals("Hello World") || greetingWithName.equals("Greetings Eduardo, welcome to Coding Dojo!")){
             System.out.println("Test successful");
         } else{
             System.out.println("Test Fail");
