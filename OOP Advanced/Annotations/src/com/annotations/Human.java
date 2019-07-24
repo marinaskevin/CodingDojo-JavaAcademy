3fc8f1d1e1575f8662f7afbeb88e367ef7e4311d
Kevin Marinas <marinaskevin@outlook.com>
2019-07-23 23:17:16 -0700

    Created and compiled Developer.java

---
+++ OOP Advanced/Annotations/src/com/annotations/Human.java
@@ -0,0 +1,11 @@
+package com.annotations;
+
+public class Human {
+    private String name;
+    public String getName(){
+        return name;
+    }
+    public void setName(String n){
+        name = n;
+    }
+}
