403ec39e26f5e1537691a0b7096bbe900a436f74
Kevin Marinas <marinaskevin@outlook.com>
2019-07-28 23:17:55 -0700

    Created and compiled TrieTest.java

---
+++ OOP Data Structures/Tries/src/com/tries/TrieTest.java
@@ -0,0 +1,15 @@
+package com.tries;
+
+public class TrieTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Trie trie = new Trie();
+        trie.insertWord("car");
+        trie.insertWord("card");
+        trie.insertWord("chip");
+        trie.insertWord("trie");
+        trie.insertWord("try");
+	}
+
+}
