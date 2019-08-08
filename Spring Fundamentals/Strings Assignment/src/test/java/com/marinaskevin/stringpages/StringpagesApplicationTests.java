eddad0d5ecbc0082e71460d10334b019cf6b45f9
Kevin Marinas <marinaskevin@outlook.com>
2019-08-07 23:35:19 -0700

    Created a StringPages program with Spring Boot

---
+++ Spring Fundamentals/Strings Assignment/src/test/java/com/marinaskevin/stringpages/StringpagesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.marinaskevin.stringpages;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StringpagesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
