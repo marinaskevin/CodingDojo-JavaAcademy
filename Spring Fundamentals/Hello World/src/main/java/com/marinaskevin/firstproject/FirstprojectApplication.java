8bcfed62530df69994946395d44efea1b51409c8
Kevin Marinas <marinaskevin@outlook.com>
2019-08-08 23:10:55 -0700

    Added RestController routes in the HelloWorld project

--- Spring Fundamentals/Hello World/src/main/java/com/marinaskevin/firstproject/FirstprojectApplication.java
+++ Spring Fundamentals/Hello World/src/main/java/com/marinaskevin/firstproject/FirstprojectApplication.java
@@ -6,15 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class FirstprojectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstprojectApplication.class, args);
 	}
 
-	@RequestMapping("/")
-	public String hello() {
-		return "Hello World";
-	}
 }
