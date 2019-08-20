8eb2fd480d9ab816819316864dd78e97f3d86342
Kevin Marinas <marinaskevin@outlook.com>
2019-08-19 23:48:28 -0700

    Created a Counter program with Spring Boot

---
+++ Spring Fundamentals/Counter/src/main/java/com/marinaskevin/counter/HomeController.java
@@ -0,0 +1,34 @@
+package com.marinaskevin.counter;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home(HttpSession session) {
+		Integer count = (Integer)session.getAttribute("count");
+		if(count == null)
+		{
+			count = 0;
+		}
+		session.setAttribute("count", count+1);
+		return "index.jsp";
+	}
+	
+	@RequestMapping("/counter")
+	public String counter(HttpSession session, Model model) {
+		Integer count = (Integer)session.getAttribute("count");
+		if(count == null)
+		{
+			count = 0;
+		}
+		model.addAttribute("visits",count);
+		return "counter.jsp";
+	}
+	
+}
---
+++ Spring Fundamentals/Counter/src/main/webapp/WEB-INF/counter.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="ISO-8859-1">
+	<title>Current visit count</title>
+</head>
+<body>
+	<h1>You have visited <a href="http://localhost:8080/">http://localhost:8080/</a> <c:out value="${visits}"/> times.</h1>
+	<h1><a href="http://localhost:8080/">Test another visit?</a></h1>
+</body>
+</html>
