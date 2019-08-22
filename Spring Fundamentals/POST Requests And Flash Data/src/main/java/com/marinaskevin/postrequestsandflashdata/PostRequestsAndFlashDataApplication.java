90e86fd9ea9ad6de436dccb48e8bb5bd7f649f07
Kevin Marinas <marinaskevin@outlook.com>
2019-08-22 00:17:16 -0700

    Refactored project with new file names and implemented Flash Data

--- Spring Fundamentals/POST Requests And Flash Data/src/main/java/com/marinaskevin/postrequestsandflashdata/PostRequestsAndFlashDataApplication.java
+++ Spring Fundamentals/POST Requests And Flash Data/src/main/java/com/marinaskevin/postrequestsandflashdata/PostRequestsAndFlashDataApplication.java
@@ -1,13 +1,13 @@
-package com.marinaskevin.springboottemplating;
+package com.marinaskevin.postrequestsandflashdata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootTemplatingApplication {
+public class PostRequestsAndFlashDataApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootTemplatingApplication.class, args);
+		SpringApplication.run(PostRequestsAndFlashDataApplication.class, args);
 	}
 
 }
